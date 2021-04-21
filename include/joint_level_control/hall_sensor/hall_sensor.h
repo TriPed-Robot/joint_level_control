19d27c1ea2e0aaa7e8b1ac63d50a2fe28c09ced5
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-04-21 10:12:03 +0200

    added more detailed description of swing sensor

--- include/joint_level_control/hall_sensor/hall_sensor.h
+++ include/joint_level_control/hall_sensor/hall_sensor.h
@@ -4,14 +4,17 @@
 #include <string>
 
 /**
- * \brief This class abstracts the communication with Sensors of the Swingjoints (currently AS504D Hall Sensors).
+ * \brief This class abstracts the communication with the sensors measuring the state of the swing Swingjoints.
+ * The measurment is provided by AS504D Hall Sensors which measure the field produced by a magnet afixed to the output lever of the motor assembly.
+ * ![hall_sensor_placement](https://raw.githubusercontent.com/TriPed-Robot/Wiki/master/pictures/hardware/swing_sensor.png)
+ * The sensors communicate via SPI and are connected according to the <a href="https://github.com/TriPed-Robot/Wiki/wiki/Wiring-diagram"> wiring diagram</a>.
  * */
 
 class HallSensor
 {
 public:    
     /**
-     * \biref Constructor of the HallSensor class
+     * \brief Constructor of the HallSensor class
      * @param SPI device, Chip select ID, mode, number of bits, communication speed and delay as well as the angle which is considered zero by the kinematics.
      **/
     HallSensor(const std::string& spi_device, uint8_t spi_cs_id, uint8_t spi_mode, uint8_t spi_bits, uint32_t spi_speed, uint16_t spi_delay, double zero_point);

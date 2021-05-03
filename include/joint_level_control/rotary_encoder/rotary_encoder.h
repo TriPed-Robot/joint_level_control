8549c700f6921f70f2d2aefc28363aa04eaaa885
Jan Baumgaertner <22688144+liquidcronos@users.noreply.github.com>
2021-05-03 10:32:23 +0000

    fixed #15

--- include/joint_level_control/rotary_encoder/rotary_encoder.h
+++ include/joint_level_control/rotary_encoder/rotary_encoder.h
@@ -26,7 +26,7 @@ public:
      * \biref Constructor of the RotaryEncoder class
      * @param SPI device, Chip select ID, mode, number of bits, communication speed and delay as well as the angle which is considered zero by the kinematics.
      **/
-    RotaryEncoder(const std::string& spi_device, uint8_t spi_cs_id, uint8_t spi_mode, uint8_t spi_bits, uint32_t spi_speed, uint16_t spi_delay, double zero_point);
+    RotaryEncoder(const std::string& spi_device, uint8_t spi_cs_id, uint8_t spi_mode, uint8_t spi_bits, uint32_t spi_speed, uint16_t spi_delay);
     ~RotaryEncoder();
    /**
      * Reads the Angle of the RotaryEncoder from the Arduino

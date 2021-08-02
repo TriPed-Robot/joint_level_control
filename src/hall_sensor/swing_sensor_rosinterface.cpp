410118a81c0d87208ba8db635e2417e808fdce95
Max Goebels <ibidumg@gmail.com>
2021-08-02 15:53:37 +0200

    added parity checking, removed mode setting

--- src/hall_sensor/swing_sensor_rosinterface.cpp
+++ src/hall_sensor/swing_sensor_rosinterface.cpp
@@ -28,6 +28,17 @@
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
+// helper function: checks if parity of recieved message is correct
+// returns 1 if parity of (rx1,rx0) is even, else returns 0.
+uint8_t checkparity(uint8_t rx1, uint8_t rx0){
+	uint16_t rx = ((uint16_t)rx1) << 8 | rx0;
+	rx ^= rx >> 8;
+	rx ^= rx >> 4;
+	rx ^= rx >> 2;
+	rx ^= rx >> 1;
+	return (~rx) & 1;
+	
+}
 
 // returns the angle the swing sensor with sensorID is measuring. Angle is in RADIANS!
 double readSwingAngle(const std::string& spi_device, uint8_t spi_cs_id, uint8_t spi_mode, uint8_t spi_bits, uint32_t spi_speed, uint16_t spi_delay)
@@ -37,7 +48,7 @@ double readSwingAngle(const std::string& spi_device, uint8_t spi_cs_id, uint8_t 
     int fd; // file descriptor
 	fd = open(spi_device.c_str(), O_RDWR); //opens SPI device, maybe put this in a once called init
  
-	uint8_t tx[] = {0xFE,0x7F}; // send buffer // sends larger indice first LIFO! //TODO: change back to 0xFF, 0xFF!!! 
+	uint8_t tx[] = {0xFF,0xFF}; // send buffer // sends larger indice first LIFO! 
 	uint8_t rx[ARRAY_SIZE(tx)] = {0, }; // recieve buffer
 	struct spi_ioc_transfer tr = {
 		.tx_buf = (unsigned long)tx,
@@ -49,28 +60,30 @@ double readSwingAngle(const std::string& spi_device, uint8_t spi_cs_id, uint8_t 
 	};
 
 	// setting spi to mode 1: CPOL=0, CPHA = 1 // TODO: for writing to extend sensor (other file), set mode there to 0!
-	int ret = ioctl(fd, SPI_IOC_WR_MODE32, &spi_mode);
+	/*int ret = ioctl(fd, SPI_IOC_WR_MODE32, &spi_mode);
 	if (ret == -1)
 		std::cout << "readSwingAngle: can't set spi mode" << std::endl;
 
 	ret = ioctl(fd, SPI_IOC_RD_MODE32, &spi_mode);
 	if (ret == -1)
 		std::cout << "readSwingAngle: can't get spi mode" << std::endl;
-	
+	*/
 
     ioctl(fd, SPI_IOC_MESSAGE(1), &tr); // transmit data over SPI to 
     close(fd); //
 	uint16_t angle = ((uint16_t) (rx[1]& 0x3F)) << 8 | rx[0];
 	double resultAngle = angle; //(((double)angle)/16384.*2*3.1415926535); // converts counts to radians
-	std::cout << "readSwingAngle: device: " << spi_device << " id: " << (int)spi_cs_id << " result: " << resultAngle << std::endl;
+	std::cout << "readSwingAngle: device: " << spi_device << " id: " << static_cast<unsigned int>(spi_cs_id) << " result: " << resultAngle << std::endl;
     
 	// checking for (parity) errors
-	bool error = rx[1] & 0x40;
+	uint16_t error = rx[1] & 0x40;
 	if (error) // error in last CMD frame (Read/Write command to sensor)
 	{
 		std::cout << "\x1b[31;4mERROR!\033[0m" << " Error bit is set!" << std::endl; 
 	}
-	
+	if(!checkparity(rx[1],rx[0])){
+		std::cout << "\x1b[31;4mERROR!\033[0m" << " Parity Error in recieved data!" << std::endl; 
+	}
 
 	return resultAngle;
 }

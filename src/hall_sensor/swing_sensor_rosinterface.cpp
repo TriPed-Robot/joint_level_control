4ad5ac220089620034666af5319508987bb7eb6d
Max Goebels <ibidumg@gmail.com>
2021-08-03 17:27:28 +0200

    removed ros debug from swing_sensor rosinterface

--- src/hall_sensor/swing_sensor_rosinterface.cpp
+++ src/hall_sensor/swing_sensor_rosinterface.cpp
@@ -65,13 +65,12 @@ double readSwingAngle(const std::string& spi_device, uint8_t spi_cs_id, uint8_t 
 	// setting spi to mode 1: CPOL=0, CPHA = 1 // TODO: for writing to extend sensor (other file), set mode there to 0!
 	int ret = ioctl(fd, SPI_IOC_WR_MODE32, &spi_mode);
 	if (ret == -1){
-		//std::cout << "readSwingAngle: can't set spi mode" << std::endl;
-		ROS_DEBUG_THROTTLE(10,"readSwingAngle: can't set spi mode (throttled message, <1 per 10s)");
+		std::cout << "readSwingAngle: can't set spi mode" << std::endl;
 	}
 		
 	ret = ioctl(fd, SPI_IOC_RD_MODE32, &spi_mode);
 	if (ret == -1){
-		ROS_DEBUG_THROTTLE(10,"readSwingAngle: can't get spi mode (throttled message, <1 per 10s)");
+		std::cout << "readSwingAngle: can't get spi mode" << std::endl;
 	}
 		
 
